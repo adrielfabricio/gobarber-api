87ff1cdb26fd337fc962a6df466021b220933a90
Adriel Fabrício <adrielfabricios@gmail.com>
2021-03-04 15:10:47 -0300

    feat: create class to handle errors

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -5,6 +5,7 @@ import { sign } from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
 import User from '../models/User';
+import AppError from '../errors/AppError';
 
 interface Request {
 	email: string;
@@ -23,13 +24,13 @@ class AuthenticateUserService {
 		const user = await userRepository.findOne({ where: { email } });
 
 		if (!user) {
-			throw Error('Incorrect email/password combination.');
+			throw new AppError('Incorrect email/password combination.', 401);
 		}
 
 		const passwordMatched = await compare(password, user.password);
 
 		if (!passwordMatched) {
-			throw Error('Incorrect email/password combination.');
+			throw new AppError('Incorrect email/password combination.', 401);
 		}
 
 		const { secret, expiresIn } = authConfig.jwt;
